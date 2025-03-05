866c57834df117cc3e40fdb2dc9fd4797fd9eced
Nikita Popkov <nikita.popkov@intervolga.ru>
2025-03-05 09:26:55 +0300

    front api test

--- server.js
+++ server.js
@@ -6,63 +6,57 @@ const TelegramBot = require('node-telegram-bot-api');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Настройка парсера для JSON
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Создание бота
-const token = '7802207571:AAEtyYS0xqzXn6K8GoYzas77qJPg5RF4Zlc'; // Замените на токен вашего бота
+const token = '7802207571:AAEtyYS0xqzXn6K8GoYzas77qJPg5RF4Zlc';
 const bot = new TelegramBot(token, { polling: true });
 
-// Данные пользователя, которые будут отображаться на странице
+let chatId = null;
 let userData = {};
+let messageHistory = [];
 
-// Обработка команды /start
 bot.onText(/\/start/, (msg) => {
-	const chatId = msg.chat.id;
-	bot.sendMessage(chatId, 'Добро пожаловать! Нажмите кнопку, чтобы отправить ваши данные.', {
-		reply_markup: {
-			inline_keyboard: [
-				[
-					{
-						text: "Отправить данные",
-						callback_data: "send_data"
-					}
-				]
-			]
-		}
-	});
+	chatId = msg.chat.id;
+	userData = {
+		id: msg.from.id,
+		first_name: msg.from.first_name,
+		last_name: msg.from.last_name || 'Нет',
+		username: msg.from.username || 'Нет'
+	};
+	bot.sendMessage(chatId, 'Добро пожаловать!');
 });
 
-// Обработка нажатия кнопки
-bot.on('callback_query', (callbackQuery) => {
-	const chatId = callbackQuery.message.chat.id;
-
-	if (callbackQuery.data === 'send_data') {
-		// Формируем данные пользователя
-		userData = {
-			id: callbackQuery.from.id,
-			first_name: callbackQuery.from.first_name,
-			last_name: callbackQuery.from.last_name || 'Не указана',
-			username: callbackQuery.from.username || 'Не указан'
-		};
-
-		// Уведомляем пользователю
-		bot.sendMessage(chatId, "Данные успешно отправлены!");
+bot.on('message', (msg) => {
+	if (msg.chat.id === chatId && msg.text) {
+		messageHistory.push({ sender: 'user', text: msg.text });
+		bot.sendMessage(chatId, "Ваше сообщение получено!");
 	}
 });
 
-// Главный маршрут
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Обработка POST запросов на '/api/data'
-app.get('/api/data', (req, res) => {
-	res.json(userData); // Отправляем данные пользователя в формате JSON
+app.get('/bot', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'bot.html'));
 });
 
-// Запуск сервера
+app.get('/api/data', (req, res) => {
+	const userInfo = { userData, messageHistory };
+	res.json(userInfo);
+});
+
+app.post('/api/message', (req, res) => {
+	const { message } = req.body;
+	if (message && chatId) {
+		messageHistory.push({ sender: 'bot', text: message });
+		bot.sendMessage(chatId, message);
+		return res.status(200).send('Сообщение отправлено в Telegram.');
+	}
+	res.status(400).send('Сообщение отсутствует или Chat ID недоступен.');
+});
+
 app.listen(PORT, () => {
 	console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
